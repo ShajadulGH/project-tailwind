1cb6a956802c1d9bc93f7eee7a3154c6287e4cdb
Shajadul <shajadulrahe@gmail.com>
2022-12-26 21:05:09 +0600

    Feat: Update to tailwinf v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  mode: "jit",
-  purge: ["*.html"],
-  darkMode: "media", // or 'media' or 'class'
+  content: ["./*.html"],
   theme: {
     extend: {
       screens: {
         sm: "540px",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
